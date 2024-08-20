d95dd274a5bcd9ec3f301f957615bd0b4912afa6
Dayananda Gowda I J <dayanandagowda2002@gmail.com>
2024-08-20 10:43:08 +0530

    Update App.java

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from CI CD task package" );
+        System.out.println( "Hello World! from CI CD task package hi from demo by jenkins" );
     }
 }
